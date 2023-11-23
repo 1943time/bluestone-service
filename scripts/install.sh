0440949a148b20aad6da4b568abfeee7567dba1b
mdwriter-09idI <mdwriter@163.com>
2023-11-24 00:18:13 +0800

    feat: v0.2.0-beta2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -OL  https://github.com/1943time/bluestone-service/releases/latest/download/bluestone-service.tar.gz
 tar zxf bluestone-service.tar.gz
+mv dist bluestone-service
 rm -rf bluestone-service.tar.gz
 cd bluestone-service
 npm i
